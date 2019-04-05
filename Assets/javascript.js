0ca14e603e6d0a10e7d5fd6d8bd90039c9bfb175
Phil Dugliss <phillipdugliss@gmail.com>
2019-04-04 20:10:59 -0500

    Commit 4

--- Assets/javascript.js
+++ Assets/javascript.js
@@ -1,13 +1,49 @@
 //define variables
 
-var userNumber
-var totalScore
-var winCount 
-var lossCount
-var crystals = [0 , 0 , 0 , 0]
+var userNumber;  //matching winning number
+var userScore;//current user value
+var winCount;  // total user wins
+var lossCount; // total user losses
+var crystals = [0 , 0 , 0 , 0];  // value of crystals
+
 
 //wire up on click events
 
+$( document ).ready(function() {
+    console.log( "ready!" );
+    reset();
+});
 
+function reset(){
+    console.log("reset");
+    userNumber = 19 + Math.floor(Math.random() * 101);
+    console.log(userNumber + "the random number maybe");
+    
+    userScore = 0;
+    
+    for(var i = 0; i < crystals.length; i++)
+        crystals[i] = 1 + Math.floor(Math.random() * 12);
+    console.log(crystals);
+}
+
+function crystalClick(crystalIndex){
+    //Apply values of crystals from the array to the total score
+    console.log("Crystal Value " + crystals[crystalIndex]);
+    userScore = crystals[crystalIndex] + userScore;
+    crystals[crystalIndex] = 1 + Math.floor(Math.random() * 12);
+    validate();
+}
+function validate(){
+    if(userScore == userNumber){
+        console.log("This is a winner");
+        reset();
+    }
+    else{
+        if(userScore > userNumber){
+        console.log( "No" , userScore , userNumber);
+        reset();
+        }   
+    }
+}
 
 
